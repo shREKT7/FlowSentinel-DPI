81e9e7e1c47ed6824eb8324318a72448827294d9
Uzair Teli <uzairteli7@gmail.com>
2026-02-26 13:17:43 +0530

    Initial commit - FlowSentinel DPI engine

---
+++ build/CMakeFiles/flowsentinel.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Projects/FlowSentinel-DPI/src/connection_tracker.cpp" "CMakeFiles/flowsentinel.dir/src/connection_tracker.cpp.obj" "gcc" "CMakeFiles/flowsentinel.dir/src/connection_tracker.cpp.obj.d"
+  "D:/Projects/FlowSentinel-DPI/src/dpi_mt.cpp" "CMakeFiles/flowsentinel.dir/src/dpi_mt.cpp.obj" "gcc" "CMakeFiles/flowsentinel.dir/src/dpi_mt.cpp.obj.d"
+  "D:/Projects/FlowSentinel-DPI/src/fast_path.cpp" "CMakeFiles/flowsentinel.dir/src/fast_path.cpp.obj" "gcc" "CMakeFiles/flowsentinel.dir/src/fast_path.cpp.obj.d"
+  "D:/Projects/FlowSentinel-DPI/src/load_balancer.cpp" "CMakeFiles/flowsentinel.dir/src/load_balancer.cpp.obj" "gcc" "CMakeFiles/flowsentinel.dir/src/load_balancer.cpp.obj.d"
+  "D:/Projects/FlowSentinel-DPI/src/packet_parser.cpp" "CMakeFiles/flowsentinel.dir/src/packet_parser.cpp.obj" "gcc" "CMakeFiles/flowsentinel.dir/src/packet_parser.cpp.obj.d"
+  "D:/Projects/FlowSentinel-DPI/src/pcap_reader.cpp" "CMakeFiles/flowsentinel.dir/src/pcap_reader.cpp.obj" "gcc" "CMakeFiles/flowsentinel.dir/src/pcap_reader.cpp.obj.d"
+  "D:/Projects/FlowSentinel-DPI/src/rule_manager.cpp" "CMakeFiles/flowsentinel.dir/src/rule_manager.cpp.obj" "gcc" "CMakeFiles/flowsentinel.dir/src/rule_manager.cpp.obj.d"
+  "D:/Projects/FlowSentinel-DPI/src/sni_extractor.cpp" "CMakeFiles/flowsentinel.dir/src/sni_extractor.cpp.obj" "gcc" "CMakeFiles/flowsentinel.dir/src/sni_extractor.cpp.obj.d"
+  "D:/Projects/FlowSentinel-DPI/src/types.cpp" "CMakeFiles/flowsentinel.dir/src/types.cpp.obj" "gcc" "CMakeFiles/flowsentinel.dir/src/types.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/flowsentinel.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flowsentinel.dir/src/connection_tracker.cpp.obj"
+  "CMakeFiles/flowsentinel.dir/src/connection_tracker.cpp.obj.d"
+  "CMakeFiles/flowsentinel.dir/src/dpi_mt.cpp.obj"
+  "CMakeFiles/flowsentinel.dir/src/dpi_mt.cpp.obj.d"
+  "CMakeFiles/flowsentinel.dir/src/fast_path.cpp.obj"
+  "CMakeFiles/flowsentinel.dir/src/fast_path.cpp.obj.d"
+  "CMakeFiles/flowsentinel.dir/src/load_balancer.cpp.obj"
+  "CMakeFiles/flowsentinel.dir/src/load_balancer.cpp.obj.d"
+  "CMakeFiles/flowsentinel.dir/src/packet_parser.cpp.obj"
+  "CMakeFiles/flowsentinel.dir/src/packet_parser.cpp.obj.d"
+  "CMakeFiles/flowsentinel.dir/src/pcap_reader.cpp.obj"
+  "CMakeFiles/flowsentinel.dir/src/pcap_reader.cpp.obj.d"
+  "CMakeFiles/flowsentinel.dir/src/rule_manager.cpp.obj"
+  "CMakeFiles/flowsentinel.dir/src/rule_manager.cpp.obj.d"
+  "CMakeFiles/flowsentinel.dir/src/sni_extractor.cpp.obj"
+  "CMakeFiles/flowsentinel.dir/src/sni_extractor.cpp.obj.d"
+  "CMakeFiles/flowsentinel.dir/src/types.cpp.obj"
+  "CMakeFiles/flowsentinel.dir/src/types.cpp.obj.d"
+  "flowsentinel.exe"
+  "flowsentinel.exe.manifest"
+  "flowsentinel.pdb"
+  "libflowsentinel.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flowsentinel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
